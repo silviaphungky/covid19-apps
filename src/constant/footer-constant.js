bfd24581afd63fb1ebc4ef6ed70b248252aaf86a
Silvia_Phungky <silvia@tinkerlust.com>
2020-08-16 18:53:03 +0700

    constant for footer

---
+++ src/constant/footer-constant.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import FaGithub from '../components/atoms/fontawesome/github/fa-github'
+import FaInstagram from '../components/atoms/fontawesome/instagram/fa-instagram'
+
+const FooterConstant =  [
+  {
+    username : 'silviaphungky',
+    icon     : <FaGithub/>,
+    href     : 'https://github.com/silviaphungky'
+  },
+  {
+    username : '_sisilp',
+    icon     : <FaInstagram/>,
+    href     : 'https://www.instagram.com/_sisilp/'
+  }
+]
+
+export default FooterConstant
